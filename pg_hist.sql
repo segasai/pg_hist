bd40926e006c4ecfaad1dd54757b10b3061b4534
Sergey Koposov <koposov@ast.cam.ac.uk>
2016-12-30 11:53:10 +0300

    add param names

--- pg_hist.sql
+++ pg_hist.sql
@@ -1,19 +1,22 @@
-CREATE or replace FUNCTION pg_hist(text, int8[], float8[], float8[]) RETURNS record
+CREATE or replace FUNCTION pg_hist(query text, nbins int8[], 
+    minimums float8[], maximums float8[]) RETURNS record
     AS '/home/koposov/pg_hist/pg_hist.so'
     LANGUAGE C;
 
-
-CREATE or replace FUNCTION pg_hist_1d(text, int8[], float8[], float8[],
-     out int , out bigint ) RETURNS setof record
+CREATE or replace FUNCTION pg_hist_1d(query text, nbins int8[], 
+     minimums float8[], maximums float8[],
+     x1 out int , hist out bigint ) RETURNS setof record
     AS '/home/koposov/pg_hist/pg_hist.so'
     LANGUAGE C;
     
-CREATE or replace FUNCTION pg_hist_2d(text, int8[], float8[], float8[],
-     out int , out int, out bigint ) RETURNS setof record
+CREATE or replace FUNCTION pg_hist_2d(query text, nbins int8[], 
+     minimums float8[], maximums float8[],
+     x1 out int , x2 out int, hist out bigint ) RETURNS setof record
     AS '/home/koposov/pg_hist/pg_hist.so'
     LANGUAGE C;
 
-CREATE or replace FUNCTION pg_hist_3d(text, int8[], float8[], float8[],
-     out int , out int, out int, out bigint ) RETURNS setof record
+CREATE or replace FUNCTION pg_hist_3d(query text, nbins int8[],
+     minimums float8[], maximums float8[],
+     x1 out int , x2 out int, x3 out int, hist out bigint ) RETURNS setof record
     AS '/home/koposov/pg_hist/pg_hist.so'
     LANGUAGE C;
